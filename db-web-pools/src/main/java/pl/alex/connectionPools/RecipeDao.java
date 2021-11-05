eb682e9ad510d07362a224eb9014c68cc6e79654
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-11-05 15:25:26 +0100

    Get connection from the context and got all info from DB

--- db-web-pools/src/main/java/pl/alex/connectionPools/RecipeDao.java
+++ db-web-pools/src/main/java/pl/alex/connectionPools/RecipeDao.java
@@ -2,15 +2,41 @@ package pl.alex.connectionPools;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipeDao {
     private final DataSource dataSource;
 
     public RecipeDao() {
-        try{
+        try {
             this.dataSource = DataSourceProvider.getDataSource();
-        }catch (NamingException e){
+        } catch (NamingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    List<Recipe> findAll() {
+        final String findQuery = "SELECT id, title, description, ingredients, preptime FROM recipe";
+        List<Recipe> recipes = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(findQuery);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("description");
+                String description = resultSet.getString("description");
+                String ingredients = resultSet.getString("ingredients");
+                int prepTime = resultSet.getInt("preptime");
+                recipes.add(new Recipe(id, title, description, ingredients, prepTime));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return recipes;
     }
 }
